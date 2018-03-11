74c9ec95d46ccc4a65cc829e327fbbe93d4b1091
Sorin Damian <sdamian@gmail.com>
2018-03-11 03:30:25 +0200

    register participant and show event page
    write some intentionally "bad" code so we can refactor it later

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,24 @@
-﻿using FiiPrezent.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FiiPrezent.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace FiiPrezent.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Event> _events = new List<Event>
+        {
+            new Event
+            {
+                Name = "Best training ever",
+                Description = "Modern web application development with ASP.NET Core :o",
+                VerificationCode = "cometothedarksidewehavecookies"
+            }
+        };
+
         // GET
         public IActionResult Index()
         {
@@ -19,7 +33,50 @@ namespace FiiPrezent.Controllers
                 return View(model);
             }
 
-            return RedirectToAction("Index");
+            // if code is valid
+            var training = _events.SingleOrDefault(x => x.VerificationCode == model.Code);
+            if (training == null)
+            {
+                ModelState.AddModelError<RsvpViewModel>(x => x.Code, "Wrong verification code");
+                return View(model);
+            }
+
+            training.RegisterParticipant(model.Name);
+
+            return RedirectToAction("Event", new
+            {
+                Id = training.Id
+            });
+        }
+
+        public IActionResult Event(string id)
+        {
+            Guid guid = Guid.Parse(id);
+            Event @event = _events.Single(x => x.Id == guid);
+            return View(new EventViewModel(@event));
+        }
+
+
+    }
+
+    public class Event
+    {
+        private readonly List<string> _participants = new List<string>();
+
+        public Event()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string VerificationCode { get; set; }
+        public string[] Participants => _participants.ToArray();
+
+        public void RegisterParticipant(string name)
+        {
+            _participants.Add(name);
         }
 
     }
---
+++ Models/EventViewModel.cs
@@ -0,0 +1,18 @@
+﻿using FiiPrezent.Controllers;
+
+namespace FiiPrezent.Models
+{
+    public class EventViewModel
+    {
+        public EventViewModel(Event @event)
+        {
+            Name = @event.Name;
+            Description = @event.Description;
+            Participants = @event.Participants;
+        }
+
+        public string Name { get; }
+        public string Description { get; }
+        public string[] Participants { get; }
+    }
+}
