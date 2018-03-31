a4aaf38fddc14ccb807882b9fe2a5eb2f809afc2
Sorin Damian <sdamian@gmail.com>
2018-03-31 22:57:36 +0300

    add admin controller and two empty views

---
+++ FiiPrezent/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FiiPrezent.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+    }
+}
